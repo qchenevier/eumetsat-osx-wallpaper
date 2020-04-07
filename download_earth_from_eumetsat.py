6b6cd3089323284d04d593ec209099fbf00ee499
Quentin Chenevier <qchenevier@users.noreply.github.com>
2020-04-07 08:55:33 +0200

    Enhance colors of the picture (less red & green, more natural to me)

--- download_earth_from_eumetsat.py
+++ download_earth_from_eumetsat.py
@@ -27,6 +27,24 @@ def add_black_margin(image_array, n_pixels):
     pad_width = ((n_pixels, n_pixels), (n_pixels, n_pixels), (0, 0))
     return np.pad(image_array, pad_width, constant_values=0)
 
+def color_mixing(image_array, mix=0.3):
+    new_image_array = image_array.copy()
+    genuine_norm = (new_image_array**2).sum(axis=2)
+
+    red = new_image_array[:, :, 0]
+    green = new_image_array[:, :, 1]
+    blue = new_image_array[:, :, 2]
+
+    new_image_array[:, :, 0] = (1-mix)*red + mix*green
+    new_image_array[:, :, 1] = (1-1.5*mix)*green + mix*red + 0.5*mix*blue
+    new_image_array[:, :, 2] = blue
+
+    norm = (new_image_array**2).sum(axis=2)
+
+    return np.nan_to_num(new_image_array * np.dstack(3*[genuine_norm]) / np.dstack(3*[norm]), 0).clip(max=1)
+
+def saturate(x, factor=2):
+    return np.tanh((x-0.5)*factor)/np.tanh(0.5*factor)/2 + 0.5
 
 #%%
 # https://github.com/domoritz/himawari-8-chrome/pull/29
@@ -42,21 +60,31 @@ image_date = (
 image_file = f"earth_{image_date}.png"
 
 #%%
+BOTTOM_CROP = 79
+BLACK_MARGIN = 50
+COLOR_MIXING = 0.3
+SATURATION_FACTOR = 1.75
+
 if not os.path.exists(image_file):
 
     mask_image_array = open_image_file("mask_manually_improved.png")[:, :, :3]
 
     image_array = download_image(image_url)
 
-    image_array = (image_array * (1 - mask_image_array / 255)).astype("uint8").clip(min=0)
-    image_array = crop_bottom_of_image(image_array, n_pixels=79)
-    image_array = add_black_margin(image_array, n_pixels=700)
+    image_array = (image_array * (1 - mask_image_array / 255)).astype("uint8").clip(min=0) / 255
+    image_array = crop_bottom_of_image(image_array, n_pixels=BOTTOM_CROP)
+    image_array = add_black_margin(image_array, n_pixels=BLACK_MARGIN)
+
+    image_array = color_mixing(image_array, mix=COLOR_MIXING)
+    image_array = saturate(image_array, factor=SATURATION_FACTOR)
+
+    image_array_int = (image_array * 255).astype(np.uint8)
 
     for f in glob.glob("earth*.png"):
         os.remove(f)
 
     from PIL import Image
-    Image.fromarray(image_array).save(image_file)
+    Image.fromarray(image_array_int).save(image_file)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 change_desktop_shell_command = (
